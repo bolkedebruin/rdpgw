5aa29af1f64c32e8efa2761e009a6145c48cf482
snowie2000 <mactype.feedback@gmail.com>
2025-01-07 18:35:27 +0800

    implement NTLM dockerfile

--- dev/docker/run.sh
+++ dev/docker/run.sh
@@ -2,31 +2,9 @@
 
 USER=rdpgw
 
-file="/root/createusers.txt"
-if [ -f $file ]
-  then
-    while IFS=: read -r username password is_sudo
-        do
-            echo "Username: $username, Password: **** , Sudo: $is_sudo"
-
-            if getent passwd "$username" > /dev/null 2>&1
-              then
-                echo "User Exists"
-              else
-                adduser -s /sbin/nologin "$username"
-                echo "$username:$password" | chpasswd
-            fi
-    done <"$file"
-fi
-
 cd /opt/rdpgw || exit 1
 
-if [ -n "${RDPGW_SERVER__AUTHENTICATION}" ]; then
-  if [ "${RDPGW_SERVER__AUTHENTICATION}" = "local" ]; then
-    echo "Starting rdpgw-auth"
-    /opt/rdpgw/rdpgw-auth &
-  fi
-fi
+/opt/rdpgw/rdpgw-auth -n rdpgw -s /tmp/rdpgw-auth.sock &
 
 # drop privileges and run the application
 su -c /opt/rdpgw/rdpgw "${USER}" -- "$@" &
